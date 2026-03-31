f8dc47665f9309d1bb7ef0e1eec5cd71d39fdfd6
Michael Bruno <michnbruno@gmail.com>
2026-03-31 15:00:09 -0400

    prod url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  apiBaseUrl: import.meta.env.VITE_API_URL || 'https://localhost:44351'
+  //apiBaseUrl: 'https://localhost:44351', // local
+  apiBaseUrl: 'https://breakfast-demo-api-hnekdkgmfed6dxa7.eastus-01.azurewebsites.net' // azure
 }
 
 export default config
